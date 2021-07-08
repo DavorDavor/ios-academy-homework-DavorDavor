9ca3c9dbf83a051387f530a39f9645c46ea71db8
Davor Lakuš <lakus.davor@gmail.com>
2021-07-08 18:31:27 +0200

    Initial project setup

---
+++ Tv Shows/Shared/Tv_ShowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tv_ShowsApp.swift
+//  Shared
+//
+//  Created by Infinum  on 08.07.2021..
+//
+
+import SwiftUI
+
+@main
+struct Tv_ShowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
