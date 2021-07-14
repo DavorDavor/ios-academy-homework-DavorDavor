8999756120c98f4c6eb4481844ba2e8db99f5bf5
Davor Lakuš <lakus.davor@gmail.com>
2021-07-14 09:10:50 +0200

    restarted project

---
+++ ShowsTV/ShowsTV/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  ShowsTV
+//
+//  Created by Infinum  on 14.07.2021..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    
+
+    
+
+
+}
+
---
+++ ShowsTV/ShowsTV/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  ShowsTV
+//
+//  Created by Infinum  on 14.07.2021..
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    
+
+
+}
+
