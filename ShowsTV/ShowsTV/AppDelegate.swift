66d4bf536d939ac84b1e238e43c61cf5d580c6a3
Davor Lakuš <lakus.davor@gmail.com>
2021-07-14 18:17:25 +0200

    fixed runtime error

--- ShowsTV/ShowsTV/AppDelegate.swift
+++ ShowsTV/ShowsTV/AppDelegate.swift
@@ -6,22 +6,34 @@
 //
 
 import UIKit
+import Alamofire
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        /*let params: [String : String] = [
+            "email" : "lakus.davor@gmail.com",
+            "password_confirmation": "infinum1",
+            "password_confirmation" : "infinum1"
+        ]
+        
+        AF
+            .request("https://tv-shows.infinum.academy/users", method: .post, parameters: params, encoder: JSONParameterEncoder.default)
+            .validate()
+            .responseJSON(completionHandler: { response in
+                result.result
+                
+            })*/
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    
-
-    
+      
 
 
 }
--- ShowsTV/ShowsTV/Login/LoginViewController.swift
+++ ShowsTV/ShowsTV/Login/LoginViewController.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+    
+    
+    var taps = 0
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("test")
+    }
+    @IBAction func IBAction(_ sender: Any) {
+        taps += 1
+        IBOutlet.text = String(taps)
+        print("Button disturbed")
+        
+        if(!activityIndicator.isAnimating){
+            activityIndicator.startAnimating()
+        } else{
+            activityIndicator.stopAnimating()
+        }
+    }
+    
+    
+    @IBOutlet weak var IBOutlet: UILabel!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
+
