20d3ef3878f27d7f05d500d115c238828f69f20b
Davor Lakuš <lakus.davor@gmail.com>
2021-07-19 12:20:28 +0200

    code organizing touches

--- ShowsTV/ShowsTV/Login/LoginViewController.swift
+++ ShowsTV/ShowsTV/Login/LoginViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class LoginViewController : UIViewController {
     
+    // MARK: - Outlets
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     
+    // MARK: - Properties
+    
+    private var numberOfClicks = 0
+    
+    //MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Actions
     @IBAction private func buttonClick(_ sender: Any) {
         numberOfClicks += 1
         titleLabel.text = String(numberOfClicks)
@@ -22,15 +35,5 @@ class LoginViewController : UIViewController {
             activityIndicator.startAnimating()
         }
     }
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
-    
-    private var numberOfClicks = 0
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    
 }
 
