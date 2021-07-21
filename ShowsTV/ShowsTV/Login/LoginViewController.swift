efcc43298f07504b084ff992030aa94710fc74fc
Davor Lakuš <lakus.davor@gmail.com>
2021-07-21 11:16:51 +0200

    minor adjustments, loginUserWith function has delayed execution for some reason

--- ShowsTV/ShowsTV/Login/LoginViewController.swift
+++ ShowsTV/ShowsTV/Login/LoginViewController.swift
@@ -5,18 +5,21 @@
 //  Created by Infinum  on 14.07.2021..
 //
 
-import Foundation
 import UIKit
+import SVProgressHUD
+import Alamofire
 
 class LoginViewController : UIViewController {
     
     // MARK: - Outlets
+    @IBOutlet private weak var emailTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
-    
-    @IBOutlet weak var rememberMeButton: UIButton!
+    @IBOutlet private weak var rememberMeButton: UIButton!
+    @IBOutlet private weak var statusLabel: UILabel!
     
     // MARK: - Properties
     private var rememberMe = false
+    private var successfulAPIcall = false
     
     //MARK: - Lifecycle Methods
     override func viewDidLoad() {
@@ -30,6 +33,7 @@ class LoginViewController : UIViewController {
             passwordTextField.isSecureTextEntry.toggle()
     }
     
+    // Toggles 'Remember me' checkbox
     @IBAction func rememberMe(_ sender: Any) {
         if rememberMe {
             rememberMeButton.setBackgroundImage(UIImage(named: "unchecked"), for: .normal)
@@ -40,21 +44,128 @@ class LoginViewController : UIViewController {
         }
     }
     
+    // triggers login API call, pushes to Home on success
     @IBAction func loginOnClick(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Home", bundle: nil)
         
-        let homeViewController = storyboard.instantiateViewController(withIdentifier:
-        "HomeViewController")
-        
-        navigationController?.pushViewController(homeViewController, animated: true)
+        // Check if fields empty
+        if emailTextField.text?.isEmpty ?? false || passwordTextField.text?.isEmpty ?? false {
+            statusLabel.text = "Please enter both email and password."
+        } else {
+            loginUserWith(email: emailTextField.text!, password: passwordTextField.text!)
+        }
+
+        if successfulAPIcall {
+            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+
+            let homeViewController = storyboard.instantiateViewController(withIdentifier:
+            "HomeViewController")
+
+            navigationController?.pushViewController(homeViewController, animated: true)
+        }
     }
+    
+    // triggers register and login API call, pushes to Home on success
     @IBAction func registerOnClick(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Home", bundle: nil)
         
-        let homeViewController = storyboard.instantiateViewController(withIdentifier:
-        "HomeViewController")
         
-        navigationController?.pushViewController(homeViewController, animated: true)
+        // Check if fields empty
+        if emailTextField.text?.isEmpty ?? false || passwordTextField.text?.isEmpty ?? false {
+            statusLabel.text = "Please enter both email and password."
+        } else {
+            registerUserWith(email: emailTextField.text!, password: passwordTextField.text!)
+        }
+        
+        if successfulAPIcall {
+            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+
+            let homeViewController = storyboard.instantiateViewController(withIdentifier:
+            "HomeViewController")
+
+            navigationController?.pushViewController(homeViewController, animated: true)
+        }
+    }
+}
+
+// MARK: login via alamofire
+
+private extension LoginViewController {
+    
+    func loginUserWith(email: String, password: String) {
+        SVProgressHUD.show()
+
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+
+        AF
+            .request(
+                "https://tv-shows.infinum.academy/users/sign_in",
+                method: .post,
+                parameters: parameters,
+                encoder: JSONParameterEncoder.default
+            )
+            .validate()
+            .responseDecodable(of: UserResponse.self) { [weak self] dataResponse in
+                switch dataResponse.result {
+                case .success(let userResponse):
+                    let headers = dataResponse.response?.headers.dictionary ?? [:]
+                    self?.handleSuccesfulLogin(for: userResponse.user, headers: headers)
+                    self?.successfulAPIcall = true
+                case .failure(let error):
+                    self?.statusLabel.text = "Login failed"
+                    SVProgressHUD.showError(withStatus: "Failure")
+                    print("Error parsing data: \(error)")
+                }
+            }
+    }
+    
+    
+}
+
+
+// MARK: register via alamofire
+
+private extension LoginViewController {
+    
+    func registerUserWith(email: String, password: String) {
+        SVProgressHUD.show()
+
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password,
+            "password_confirmation": password
+        ]
+
+        AF
+            .request(
+                "https://tv-shows.infinum.academy/users",
+                method: .post,
+                parameters: parameters,
+                encoder: JSONParameterEncoder.default
+            )
+            .validate()
+            .responseDecodable(of: UserResponse.self) { [weak self] dataResponse in
+                switch dataResponse.result {
+                case .success(let userResponse):
+                    let headers = dataResponse.response?.headers.dictionary ?? [:]
+                    self?.handleSuccesfulLogin(for: userResponse.user, headers: headers)
+                    self?.successfulAPIcall = true
+                case .failure(let error):
+                    self?.statusLabel.text = "Registration failed"
+                    SVProgressHUD.showError(withStatus: "Failure")
+                    print("Error parsing data: \(error)")
+                }
+            }
+    }
+    func handleSuccesfulLogin(for user: User, headers: [String: String]) {
+        guard let authInfo = try? AuthInfo(headers: headers) else {
+            statusLabel.text = "Missing headers"
+            SVProgressHUD.showError(withStatus: "Missing headers")
+            return
+        }
+        statusLabel.text = "Success"
+        SVProgressHUD.showSuccess(withStatus: "Success")
     }
 }
 
